a6b338949cb887ac808e391ea239c99bdc94c484
morphibius <nguyenanhtuan1232@gmail.com>
2025-02-21 01:33:33 +0700

    [ng-anhhtuann] fix mq config

--- order/mqlistener.py
+++ order/mqlistener.py
@@ -2,15 +2,21 @@ from rabbitmq import consume_message
 from sqlalchemy.orm import Session
 import models, database
 
+QUEUE_UPDATE = "order.update"
+
 def update_order_status(data):
+    
     db = database.SessionLocal()
     order = db.query(models.Order).filter(models.Order.id == data["order_id"]).first()
-    print(data)
+    
     if order:
         order.status = data["status"]
         db.commit()
     db.close()
 
+import threading
 
 def start_listener():
-    consume_message("order.update", update_order_status)
+    thread = threading.Thread(target=consume_message, args=(QUEUE_UPDATE, update_order_status))
+    thread.daemon = True
+    thread.start()
